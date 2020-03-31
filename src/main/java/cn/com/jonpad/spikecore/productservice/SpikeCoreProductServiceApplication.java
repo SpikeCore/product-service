44c755aa910c91d2ce53b15a629b71096e86f814
JonChan <jonchan.hv@outlook.com>
2020-03-31 21:47:01 +0800

    init repository

---
+++ src/main/java/cn/com/jonpad/spikecore/productservice/SpikeCoreProductServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.com.jonpad.spikecore.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeCoreProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpikeCoreProductServiceApplication.class, args);
+    }
+
+}
