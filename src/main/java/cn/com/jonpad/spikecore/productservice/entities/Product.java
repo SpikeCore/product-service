780e205a50e0d0063c13d75a3c77577ca7e4e370
JonChan <jonchan.hv@outlook.com>
2020-03-31 23:33:26 +0800

    引入Redis + JPA

---
+++ src/main/java/cn/com/jonpad/spikecore/productservice/entities/Product.java
@@ -0,0 +1,12 @@
+package cn.com.jonpad.spikecore.productservice.entities;
+
+import java.io.Serializable;
+
+/**
+ * 产品
+ * @author Jon Chan
+ * @date 2020/3/31 23:27
+ */
+public class Product implements Serializable {
+
+}
